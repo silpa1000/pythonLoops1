2ece3566355ab542c6fdbf2d921fa9a3cdb6084c
silpa1000 <100128518+silpa1000@users.noreply.github.com>
2022-04-06 12:10:10 +0530

    Add files via upload
    # Python program to demonstrate star pattern

---
+++ pyramid pattern2.py
@@ -0,0 +1,16 @@
+# Python program to demonstrate star pattern
+def pypart2(n):
+    k = 2 * n - 2
+    for i in range(0, n):
+
+
+        for j in range(0, k):
+            print(end=" ")
+        k = k - 2
+
+        for j in range(0, i + 1):
+            print("* ", end="")
+
+        print("\r")
+n = 5
+pypart2(n)
