3e478e9279f09a6cfc7f5b003aff74b234fccc15
silpa1000 <100128518+silpa1000@users.noreply.github.com>
2022-04-06 12:11:12 +0530

    Add files via upload
    # Write a program that appends the square of each number to a new list

---
+++ squared numbers.py
@@ -0,0 +1,10 @@
+# Write a program that appends the square of each number to a new list
+
+
+numbers = [1, 2, 3, 4, 5]
+
+
+squared_numbers = [number ** 2 for number in numbers]
+
+
+print(squared_numbers)
